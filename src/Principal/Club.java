417320cae5a3f8d5d09e055389d3677afeaf9d96
Moises <moisescamposdaw@gmail.com>
2024-04-26 19:06:03 +0100

    Cambio de funcionamiento para Map

--- src/Principal/Club.java
+++ src/Principal/Club.java
@@ -1,10 +1,8 @@
 
 package Principal;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -19,7 +17,7 @@ public class Club {
     /**
      * Colección de tipo set
      */
-    private Set<Socio> plantilla;
+    private Map<String, Socio> plantilla;
 
     
     /*     
@@ -29,7 +27,7 @@ public class Club {
     */
     public Club(String nombre){
         setNombre(nombre);
-        this.plantilla = new TreeSet<>();
+        this.plantilla = new TreeMap<>();
     }
 
     
@@ -40,128 +38,6 @@ public class Club {
     */
     
     
-    /**
-     * Crea una nueva colección de forma local, aplica el orden por nombre y luego se establece como "plantilla"
-     * Usa Clase anónima que establece un comparador por nombres de socios para poder ordernarlos según ese criterio
-     */
-    public void ordenarPorNombre(){ // CORRECCIÓN: Es mejor hacer los comparator en otra clase    
-        Set<Socio> nuevoOrden = new TreeSet<>( // CORRECCIÓN: Esto va en la clase Socio
-           new Comparator<>(){
-                @Override
-                public int compare(Socio o1, Socio o2) {
-                    return o1.getNombre().compareTo(o2.getNombre());
-                }            
-           } 
-        );
-        
-        nuevoOrden.addAll(plantilla);
-        this.plantilla = nuevoOrden;
-    }
-    
-    
-    /**
-     * Crea una nueva colección de forma local, aplica el orden por nombre y luego se establece como "plantilla"
-     * Usa Clase anónima que establece un comparador por antigüedad de socios para poder ordernarlos según ese criterio
-     */
-    public void ordenarPorAntiguedad(){ // CORRECCIÓN: Es mejor hacer los comparator en otra clase
-        Set<Socio> nuevoOrden = new TreeSet<>( // CORRECCIÓN: Se debe hacer la clase anónima por separado, no dentro del constructor de la colección
-            new Comparator<>(){
-                @Override
-                public int compare(Socio o1, Socio o2) {
-                    return o1.antiguedad()-o2.antiguedad();
-                }
-            }
-        );
-        
-        nuevoOrden.addAll(plantilla);
-        this.plantilla = nuevoOrden;
-    
-    }
-    
-    /**
-     * Con el objetivo de eliminar un socio del club
-     * @param dni del socio a modificar
-     */
-    public void darBaja(String dni){
-        Iterator<Socio> it = this.plantilla.iterator(); // CORRECIÓN: Se puede hacer sin iterator
-        
-        boolean encontrado=false;
-        
-        while (it.hasNext()) {
-            Socio socio = it.next();
-            if (socio.getDni().equals(dni)) {
-                it.remove();               
-                encontrado = true;
-                break;
-            }
-        }
-        
-        if (encontrado==false) {
-            System.out.println("Objeto no encontrado");
-        }
-    }
-    
-    
-    /**
-     * Con el objetivo de agregar un nuevo socio al club
-     * @param socio objeto para agregar al colección "plantilla"
-     */
-    public void darAlta(Socio socio){ // CORRECIÓN: Hizo falta un reutrn boolean para saber si se agregó o no
-        this.plantilla.add(socio);
-    }
-    
-    
-    /**
-     * Busca el dni dentro de la colección y si hay coincidencia aplica la modificación del nombre
-     * @param dni del socio a modificar
-     * @param nuevoNombre principal del socio
-     */
-    public void modificarNombre(String dni, String nuevoNombre){ // CORRECIÓN: Se puede hacer sin iterator
-
-        Iterator<Socio> it = this.plantilla.iterator();
-        
-        boolean encontrado=false;
-        
-        while (it.hasNext()) {
-            Socio socio = it.next();
-            if (socio.getDni().equals(dni)) {
-                socio.cambiarNombre(nuevoNombre);
-                encontrado = true;
-                break;
-            }
-        }
-        
-        if (encontrado==false) {
-            System.out.println("Objeto no encontrado");
-        }
-    }
-    
-    
-    /**
-     * Busca el dni dentro de la colección y si hay coincidencia aplica la modificación de la fecha de alta
-     * @param dni del socio a modificar
-     * @param nuevaFecha de alta para el socio
-     */
-    public void modificarFechaAlta(String dni, String nuevaFecha){ // CORRECIÓN: Se puede hacer sin iterator
-
-        Iterator<Socio> it = this.plantilla.iterator();
-        
-        boolean encontrado=false;
-        
-        while (it.hasNext()) {
-            Socio socio = it.next();
-            if (socio.getDni().equals(dni)) {
-                socio.cambiarFechaAlta(nuevaFecha);
-                encontrado = true;
-                break;
-            }
-        }
-        
-        if (encontrado==false) {
-            System.out.println("Objeto no encontrado");
-        }
-    }  
-    
     
     /*
     *
@@ -170,12 +46,8 @@ public class Club {
     */
     
     @Override
-    public String toString(){
-        String detalles="";
-        for (Socio socio : plantilla) {
-            detalles += socio;
-        }
-        return detalles;
+    public String toString(){ // ---- PENDIENTE ----
+        return "";
     }
     
     
@@ -202,11 +74,4 @@ public class Club {
         this.nombre = nombre;
     }
 
-    /**
-     * Colección de tipo set
-     * @return the plantilla del club
-     */
-    public Set<Socio> getPlantilla() {
-        return plantilla;
-    }
 }
