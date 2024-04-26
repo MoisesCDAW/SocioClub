a622271b8728e4f1a46f914d53efaf0c3b45b25a
MoisesCDAW <moisescamposdaw@gmail.com>
2024-04-26 12:35:20 +0100

    Modifiqué la estructura de la muestra de resultados de las pruebas y ya no están las clases anónimas porque fueron trasladadas a la clase socio

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,13 +1,6 @@
 
 package Principal;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
-
-
-
 /**
  *
  * @author MoisesC
@@ -20,58 +13,50 @@ public class Main {
      * @param args Array de tipo String
      */
     public static void main(String[] args) {
-        
-        /**
-         * Clase anónima que establece un comparador por nombres de socios para poder ordernarlos según ese criterio
-         */
-        Comparator<Socio> comparaNombre = new Comparator<>(){
-            @Override
-            public int compare(Socio o1, Socio o2) {
-                return o1.getNombre().compareTo(o2.getNombre());
-            }
-            
-        };
-        
-        /**
-         * Clase anónima que establece un comparador por antigüedad de socios para poder ordernarlos según ese criterio
-         */
-        Comparator<Socio> comparaAntiguedad = new Comparator<>(){
-            @Override
-            public int compare(Socio o1, Socio o2) {
-                return o1.antiguedad()-o2.antiguedad();
-            }
-            
-        };
-        
+     
         Club cs1 = new Club("Nautico");
         
+        Socio s3 = new Socio("14","Campos","01/02/2021");
         Socio s1 = new Socio("12","Moisés","01/02/2023");
         Socio s2 = new Socio("11","Alejandro","01/02/2022");
-        Socio s3 = new Socio("14","Campos","01/02/2021");
-        Socio s4 = new Socio("12","Moisés","01/02/2023"); // NO lo está agregando porque está repetido
-  
-        cs1.darAlta(s1);
-        cs1.darAlta(s2);
+        Socio s4 = new Socio("10","Moi","01/02/2020"); 
+        
+        /* NOTAS:
+        * Si coloco DNI = 9, al ordenar lo coloca al final
+        * Si coloco dos socios iguales solo se agrega uno, pero podré usar cualquier variable de referencia
+        */
+        
+
+        System.out.println("---- ALTA DE VARIOS SOCIOS ---");
         cs1.darAlta(s3);
+        cs1.darAlta(s1);
+        cs1.darAlta(s2);      
         cs1.darAlta(s4);
+        System.out.println(cs1+"\n");
         
-        System.out.println(cs1);
-        System.out.println();
+
+        System.out.println("---- SOCIO DADO DE BAJA ---");
+        cs1.darBaja(s4.getDni());
+        System.out.println(cs1+"\n");
         
-        cs1.darBaja(s4.getDni()); // Funcionará tanto con s1 como con s4
         
-        System.out.println(cs1);
-        System.out.println();
-        
+        System.out.println("---- CAMBIO DE NOMBRE ---");
         cs1.modificarNombre(s2.getDni(), "Ale");
+        System.out.println(cs1+"\n");
         
-        System.out.println(cs1);
-        System.out.println();
-        
+             
+        System.out.println("---- CAMBIO DE ANTIGÜEDAD ---");
         cs1.modificarFechaAlta(s2.getDni(), "01/01/1995");
+        System.out.println(cs1+"\n");
         
-        System.out.println(cs1);
-        System.out.println();
+   
+        System.out.println("---- ORDENADO POR ANTIGÜEDAD ---");
+        cs1.ordenarPorAntiguedad();
+        System.out.println(cs1+"\n");
+        
+        System.out.println("---- ORDENADO POR NOMBRE ---");
+        cs1.ordenarPorNombre();
+        System.out.println(cs1+"\n");
     }
     
 }
